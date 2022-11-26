511933c3291ac4e7eace7162103c5fae0e857d37
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2022-11-26 01:58:46 +0100

    Fix texture path choice

--- services/skyTexture.lua
+++ services/skyTexture.lua
@@ -5,7 +5,7 @@ local skyTexture = {}
 local common = require("tew.Watch the Skies.components.common")
 local debugLog = common.debugLog
 local config = require("tew.Watch the Skies.config")
-local WtSdir = "Data Files\\Textures\\tew\\Watch the Skies\\"
+local WtSdir = "Data Files\\Textures\\tew\\Watch the Skies"
 local WtC = tes3.worldController.weatherController
 
 --------------------------------------------------------------------------------------
@@ -45,33 +45,45 @@ function skyTexture.randomise()
 	end
 	if (WtC.nextWeather) then return end
 
-	-- Check against config chances, do nothing if dice roll determines we should use vanilla instead --
 	debugLog("Starting cloud texture randomisation.")
 	for index, weather in ipairs(WtC.weathers) do
 		if (weatherNow) and (weatherNow.index == index) then goto continue end
-			weather.cloudTexture = WtSdir .. weather.name:lower() .. "\\" .. table.choice(weathers.customWeathers[index-1])
-			debugLog("Cloud texture path set to: " .. weather.name .. " >> " .. weather.cloudTexture)
+		local texturePath = table.choice(weathers.customWeathers[index-1])
+		weather.cloudTexture = texturePath
+		debugLog("Cloud texture path set to: " .. weather.name .. " >> " .. weather.cloudTexture)
 		::continue::
 	end
 end
 
 function skyTexture.init()
 	-- Populate data tables with cloud textures --
-	for weather, index in pairs(tes3.weather) do
-		debugLog("Weather: " .. weather)
-		for sky in lfs.dir(WtSdir .. weather) do
+	for name, index in pairs(tes3.weather) do
+		for sky in lfs.dir(WtSdir .. "\\" .. name) do
 			if sky ~= ".." and sky ~= "." then
 				if string.endswith(sky, ".dds") or string.endswith(sky, ".tga") then
-					table.insert(weathers.customWeathers[index], sky)
-					debugLog("File added: " .. sky)
+					local texturePath = string.format(
+						"%s\\%s\\%s",
+						WtSdir,
+						name,
+						sky
+					)
+					table.insert(weathers.customWeathers[index], texturePath)
+					debugLog("File added: " .. texturePath)
 				end
 			end
 		end
 	end
 
+	-- Also pull vanilla textures if needed --
 	if config.useVanillaSkyTextures then
 		for index, sky in ipairs(weathers.vanillaWeathers) do
-			table.insert(weathers.customWeathers[index], sky)
+			local texturePath = string.format(
+				"%s\\%s",
+				"Data Files\\Textures",
+				sky
+			)
+			table.insert(weathers.customWeathers[index], texturePath)
+			debugLog("File added: " .. texturePath)
 		end
 	end
 
